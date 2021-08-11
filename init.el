a62f35d57c0d497c7588afbd073d0de3fbb73194
Jesse Lucas <jlucas@example.org>
2021-08-11 19:29:26 -0400

    Add init.el

---
+++ init.el
@@ -0,0 +1,87 @@
+;; Minimal config for Emacs 24.3
+;; 24.3 is the version of Emacs in RHEL7 and other older Linux distributions
+
+;;
+;; Enabled commands
+;;
+
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+
+;;
+;; Basic preferences
+;;
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;
+;; Temp directory, autosaves, and backups
+;;
+
+(defconst my-temp-dir (format "~/tmp/emacs-%s.%s/"
+			      emacs-major-version
+			      emacs-minor-version))
+(make-directory my-temp-dir t)
+(custom-set-variables
+ '(auto-save-file-name-transforms `((".*" ,my-temp-dir t)))
+ '(backup-directory-alist `((".*" . ,my-temp-dir))))
+(setq create-lockfiles nil)
+
+;;
+;; Set up load-path
+;;
+
+(let ((default-directory "~/.emacs.d/elisp/"))
+  (add-to-list 'load-path default-directory)
+    (normal-top-level-add-subdirs-to-load-path))
+
+;;
+;; Set up package
+;;
+
+(require 'package)
+
+;; MELPA setup instructions from
+;; https://melpa.org/#/getting-started
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; Add Magit v1 package archive to support old Emacs 24.3
+;; https://magit.vc/elpa/v1/
+(add-to-list 'package-archives
+             '("magit-v1" . "https://magit.vc/elpa/v1/packages/") t)
+;; (add-to-list 'package-pinned-packages '(magit . "magit-v1"))
+
+;; Initialize the package package
+(package-initialize)
+
+(defadvice package-compute-transaction
+		   (before package-compute-transaction-reverse (package-list requirements) activate compile)
+		   "reverse the requirements"
+		   (setq requirements (reverse requirements))
+		   (print requirements))
+
+;;
+;; Magit setup
+;;
+
+(setq magit-last-seen-setup-instructions "1.4.0")
+(setq magit-auto-revert-mode t)
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;;
+;; Key binds
+;;
+
+;; C-SPC is my tmux prefix!
+;; You can get M-SPC #'just-one-space functionality with M-\
+;; Git Bash on Windows has an option to disable Alt-Space toggling the window menu
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+(global-set-key (kbd "C-x C-k") 'kill-region) ; instead of default bind to C-w
+                                              ; usually bound to 'edit-kbd-macro
+(global-set-key (kbd "C-w") 'backward-kill-word) ; as in the shell, vim. etc.
+(global-set-key (kbd "M-%") 'replace-regexp) ; do i ever not want this?
+
