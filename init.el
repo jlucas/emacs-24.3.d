34845cac11b6afe67b63496e8f3c640a792c420b
Jesse Lucas <jlucas@example.org>
2021-08-12 12:47:05 -0400

    Add paredit mode
    paredit.el from http://mumble.net/~campbell/emacs/paredit.el

--- init.el
+++ init.el
@@ -103,6 +103,18 @@
 (global-set-key (kbd "C-c u") 'undo-tree-visualize)
 
 ;;
+;; Paredit
+;;
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;;
 ;; Key binds
 ;;
 
