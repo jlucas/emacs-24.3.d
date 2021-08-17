c4bc95f376b21f68fc5038d8c50062dafda1088e
Jesse Lucas <jlucas@example.org>
2021-08-17 14:55:02 -0400

    Set file-name-handler-alist to nil for init
    Another recommendation from
    https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
 ;; Load my setup
 (setq gc-cons-threshold 100000000)
-(load (concat (file-name-directory load-file-name) "user.el"))
-
+(let ((file-name-handler-alist nil))
+  (load (concat (file-name-directory load-file-name) "user.el")))
